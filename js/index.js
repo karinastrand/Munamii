2c7b3d2489c653603fea9475177edece6ff161fc
kariastrand <karinastrand@yahoo.se>
2024-03-01 15:06:20 +0100

    before comments and cleaning up

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function shopDropdown() {
+    document.getElementById("dropdownList").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function addToCart(text1,text2)
+  {
+    
+    alert(text1+", price "+text2 +" was added to your cart");
+  }
